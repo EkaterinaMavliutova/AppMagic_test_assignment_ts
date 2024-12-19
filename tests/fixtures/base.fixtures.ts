504521ef1a5458b05236bfd7ec91dbf1f1ac3d5f
ekaterinamavliutova <akumaaya@gmail.com>
2024-12-19 18:57:43 +0100

    refactor utils and fixtures

--- tests/fixtures/base.fixtures.ts
+++ tests/fixtures/base.fixtures.ts
@@ -1,8 +1,7 @@
 import { test as base } from '@playwright/test';
 import AppMagic from '../pages/AppMagic';
-import BasePage from 'tests/pages/base.page';
 
-export const test = base.extend<{ appMagic: AppMagic }>({
+const test = base.extend<{ appMagic: AppMagic }>({
   appMagic: async ({ page }, use) => {
     const appMagic = new AppMagic(page);
     await page.goto('./top-charts/apps');
@@ -10,18 +9,4 @@ export const test = base.extend<{ appMagic: AppMagic }>({
   },
 });
 
-export function step(stepName?: string) {
-  return function decorator(
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    target: (args?: any) => Promise<any>,
-    context: ClassMethodDecoratorContext,
-  ) {
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    return function replacementMethod(this: BasePage, ...args: any) {
-      const name = stepName || context.name as string;
-      return test.step(name, async () => {
-        return await target.call(this, ...args);
-      });
-    };
-  };
-};
+export default test;
--- tests/utils/utils.ts
+++ tests/utils/utils.ts
@@ -1,6 +1,8 @@
 import { Locator, TestInfo } from "@playwright/test";
+import BasePage from "tests/pages/base.page";
+import test from '../fixtures/base.fixtures';
 
-const attachScreenshotToReport = async (locator: Locator, name: string, testInfo: TestInfo) => {
+export const attachScreenshotToReport = async (locator: Locator, name: string, testInfo: TestInfo) => {
   const screenshot = await locator.screenshot();
 
   await testInfo.attach(name, {
@@ -9,4 +11,18 @@ const attachScreenshotToReport = async (locator: Locator, name: string, testInfo
   });
 };
 
-export default attachScreenshotToReport;
+export function step(stepName?: string) {
+  return function decorator(
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    target: (args?: any) => Promise<any>,
+    context: ClassMethodDecoratorContext,
+  ) {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    return function replacementMethod(this: BasePage, ...args: any) {
+      const name = stepName || context.name as string;
+      return test.step(name, async () => {
+        return await target.call(this, ...args);
+      });
+    };
+  };
+};
